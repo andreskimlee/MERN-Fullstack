cec31e3636a0c64c10b8ef4c920265e02d930d87
Andres Kim <andreskimlee@gmail.com>
2020-01-09 19:11:37 -0500

    Server Connect with DB

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+const mongoose = require('mongoose');
+
 const express = require("express"); // initializes and creates server 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Helloss World"));
 
-const port = process.env.PORT || 6000;
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
 
 
