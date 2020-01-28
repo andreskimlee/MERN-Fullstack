1e8a5bd35dba3957523295b745e8760f70115c5b
Andres Kim <andreskimlee@gmail.com>
2020-01-28 11:54:26 -0500

    heroku fix

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
-const mongoose = require('mongoose');
-const users = require("./routes/api/users");
-const photos = require("./routes/api/photos");
-const express = require("express"); // initializes and creates server 
-const app = express();
-
+const express = require("express")
 const bodyParser = require('body-parser'); // parse JSON 
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express();
+const mongoose = require("mongoose")
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// Socket.io
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('User Disconnected');
+  });
+  socket.on('example_message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+io.listen(8000);
 
+
+let clients = 0
+
+io.on('connection', function (socket) {
+    socket.on("NewClient", function () {
+        if (clients < 2) {
+            if (clients == 1) {
+                this.emit('CreatePeer')
+            }
+        }
+        else
+            this.emit('SessionActive')
+        clients++;
+    })
+    socket.on('Offer', SendOffer)
+    socket.on('Answer', SendAnswer)
+    socket.on('disconnect', Disconnect)
+})
+
+function Disconnect() {
+    if (clients > 0) {
+        if (clients <= 2)
+            this.broadcast.emit("Disconnect")
+        clients--
+    }
+}
+
+function SendOffer(offer) {
+    this.broadcast.emit("BackOffer", offer)
+}
+
+function SendAnswer(data) {
+    this.broadcast.emit("BackAnswer", data)
+}
 
 app.get("/", (req, res) => res.send("Helloss World"));
 
@@ -17,9 +61,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-app.use("/api/users", users);
-app.use("/api/photos", photos); 
+
+
